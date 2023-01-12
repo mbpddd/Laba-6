412cf3219ee67f55a2624dd2cc649aea79cad895
mirosastav02@gmail.com <mirosastav02@gmail.com>
2023-01-12 09:37:16 +0200

    Laba-6

---
+++ laba6/laba6/laba6.cpp
@@ -0,0 +1,26 @@
+﻿
+
+#include <iostream>
+using namespace std;
+struct Auto{
+	int wheels;
+	float speed;
+	char color;
+	float value;
+};
+
+int main()
+{
+	Auto bugatti;
+	bugatti.color = 'b';
+	bugatti.speed = 415.62;
+	bugatti.wheels = 4;
+	bugatti.value = 6.2;
+
+	Auto bmw = { 4,320.43,'red',3.5 };
+	cout << "BMW`s speed = " << bmw.speed<< " " << "BMW`s value = " << bmw.value << "\n" << "bugatti`s speed = " << bugatti.speed<< " " << "bugatti`s value = " << bugatti.value << endl;
+
+
+
+}
+
